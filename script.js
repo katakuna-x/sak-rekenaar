975ffba6f2142e9f7d1fa2bd6e67bc658b803cd0
alex <coetzeebornman.alex@gmail.com>
2025-02-08 19:55:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
